c059d774098e9cd097403909fc8b40356dff502b
Maboroshi0327 <maboroshi0327@gmail.com>
2025-02-18 16:36:54 +0800

    RTNSTV: Add infer.py.

---
+++ Real-Time-Neural-Style-Transfer-for-Videos/infer.py
@@ -0,0 +1,14 @@
+import cv2
+import torch
+
+from utilities import Inference
+from network import StylizingNetwork
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    inference = Inference(StylizingNetwork, "./models/RTNSTV_epoch_1_batchSize_2.pth", "../datasets/video2.mp4", device)
+    for output in inference:
+        cv2.imshow("Frames", output)
+        if cv2.waitKey(30) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
