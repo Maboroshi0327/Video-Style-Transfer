5dfd277e3658e20d648fdc8acf8e137e0c8f638b
Maboroshi0327 <maboroshi0327@gmail.com>
2025-02-23 04:37:05 +0800

    RTNSTV: Add experiment_Et.py.

---
+++ Real-Time-Neural-Style-Transfer-for-Videos/experiment_Et.py
@@ -0,0 +1,9 @@
+import torch
+
+from network import StylizingNetwork
+from utilities import temporal_errors_sintel
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+error = temporal_errors_sintel(StylizingNetwork, "./models/RTNSTV_epoch_2_batchSize_2.pth", "alley_1", device)
+print(error)
