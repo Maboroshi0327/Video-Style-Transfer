27f9f3680e1a1063a4fdc13c7e487948b21e7c9e
Maboroshi0327 <maboroshi0327@gmail.com>
2025-03-02 23:51:50 +0800

    ReCoNet: Update some files.

--- Real-time-Coherent-Video-Style-Transfer-Network-(ReCoNet)/inference_two_model/infer.py
+++ Real-time-Coherent-Video-Style-Transfer-Network-(ReCoNet)/inference_two_model/infer.py
@@ -9,12 +9,12 @@ from utilities import Inference
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model_1_infer = Inference(ReCoNet, 1, "./models/starry-night-1.pth", "../datasets/video3.mp4", device)
-    model_4_infer = Inference(ReCoNet, 1, "./models/starry-night-2.pth", "../datasets/video3.mp4", device)
+    model_10_infer = Inference(ReCoNet, 1, "./models/Flow_input_1_epoch_10_batchSize_2.pth", "../datasets/video3.mp4", device)
+    model_4_infer = Inference(ReCoNet, 1, "./models/Flow_input_1_epoch_4_batchSize_2.pth", "../datasets/video3.mp4", device)
 
-    for output_image_1, output_image_4 in zip(model_1_infer, model_4_infer):
-        cv2.imshow("Input Frames =  1", output_image_1)
-        cv2.imshow("Input Frames =  4", output_image_4)
+    for output_image_10, output_image_4 in zip(model_10_infer, model_4_infer):
+        cv2.imshow("10", output_image_10)
+        cv2.imshow("4", output_image_4)
         if cv2.waitKey(10) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
